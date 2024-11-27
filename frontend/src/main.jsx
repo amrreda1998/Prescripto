418b2595ec59e3b31ff0730b3a17d23a94d4fe4c
Amr Reda <amr.redab1398@gmail.com>
2024-11-27 20:19:40 +0200

    fixing main.jsx file for vercel deploy

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 import { TokenProvider } from './context/tokenCotextProvider.jsx';
-import ScrollToTop from './components/scrollToTop.jsx';
+import ScrollToTop from './components/ScrollToTop.jsx';
 import { AppointmentsProvider } from './context/AppointmentsContextProvider.jsx';
 import { UserProvider } from './context/userContextProvider.jsx';
 import { DoctorsProvider } from './context/doctorsContextProvider.jsx';
