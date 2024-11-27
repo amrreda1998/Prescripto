36d4e90230cbab7d4dbd136f1ba2e62be958635e
Amr Reda <amr.redab1398@gmail.com>
2024-11-27 20:16:03 +0200

    fixing main.jsx file for vercel deploy

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,11 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
-import { TokenProvider } from './context/tokenCotextProvider';
+import { TokenProvider } from './context/tokenCotextProvider.jsx';
 import ScrollToTop from './components/scrollToTop.jsx';
 import { AppointmentsProvider } from './context/AppointmentsContextProvider.jsx';
 import { UserProvider } from './context/userContextProvider.jsx';
-import { DoctorsProvider } from './context/doctorsContextProvider';
+import { DoctorsProvider } from './context/doctorsContextProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <TokenProvider>
