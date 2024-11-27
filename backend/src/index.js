13e461cf677440effbedcc30497a8b41f2620d0c
Amr Reda <amr.redab1398@gmail.com>
2024-11-27 21:51:29 +0200

    modify cors code to fix cors Error

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,13 +15,24 @@ const port = process.env.PORT || 4000;
 mongoDbConnect();
 configureCloudinary();
 
-//middlewares
+// Middlewares
+const allowedOrigins = ['https://prescripto-book-doctors.vercel.app'];
 app.use(
   cors({
-    origin: 'https://prescripto-book-doctors.vercel.app',
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps, curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     credentials: true,
   })
 );
+
 app.use(express.json());
 
 //call seed function for doctors data
